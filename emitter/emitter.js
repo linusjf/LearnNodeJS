07e008140abc3e3da6d07b6e4332985285fed2fd
fernal73 <linus.fernandes@gmail.com>
2019-04-03 17:58:52 +0530

    Added code to allow user to specify regular expression search criteria.

--- emitter/emitter.js
+++ emitter/emitter.js
@@ -1,11 +1,33 @@
-if (process.argv.length > 2)
-    var RegularExp = new RegExp(process.argv[2]);
+var argc  = process.argv.length;
+var expression;
+
+function parseArgs(noOfArgs)
+{
+if (noOfArgs  > 2)
+{
+    let expr = process.argv[2];
+    if (noOfArgs > 3)
+    {
+        let options = process.argv[3];
+        let pattern = new RegExp("^-(?=[gim]{1,3}$)(?!.*(.).*\1).*$");
+        if (!pattern.test(options))
+            exitMessage();
+        else
+            expression = new RegExp(expr,options.substring(1));
+    }
+    else
+        expression = new RegExp(expr);
+
+}
 else
         exitMessage();
+}
+
+parseArgs(argc);
 
 function exitMessage()
     {
-    console.error("Usage: node emitter.js 'regularexpression'");
+    console.error("Usage: node emitter.js 'regularexpression' -[g][i][m]\ng - global,i - case-insensitive,m - multiline");
     process.exit(1);
 
     }
@@ -38,9 +60,11 @@ fs.readdir(".",function(err,files)
         if (err)
             console.log("Error reading directory: "+err.message);
         else
-    {
+
+        {
 /**findPattern(files, /hello \w+/g)***/
-findPattern(files,RegularExp)
+files.push('nonexistentfile.txt');
+findPattern(files,expression)
     .on('fileread', function(file) { console.log(file + ' was read'); })
     .on('found',
         function(
