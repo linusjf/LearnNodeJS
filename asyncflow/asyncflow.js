bce597e723b900705d618e971fe8bb4d171bcd85
fernal73 <linus.fernandes@gmail.com>
2019-04-24 13:37:19 +0530

    Added asyncflow.js. Updated .gitignore

---
+++ asyncflow/asyncflow.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function asyncFlow(generatorFunction) {
+    function callback(err) {
+        if (err) {
+            return generator.throw(err);
+        }
+        var results = Array.prototype.slice.call(arguments, 1);
+        generator.next(results.length > 1 ? results : results[0]);
+    };
+    var generator = generatorFunction(callback);
+    generator.next();
+}
+
+var fs = require('fs');
+var path = require('path');
+
+
+asyncFlow(function*(callback) {
+    var fileName = path.basename(__filename);
+    var myself = yield fs.readFile(fileName, 'utf8', callback);
+  yield fs.writeFile('clone_of_' + fileName, myself, callback);
+    console.log('Clone created');
+});
