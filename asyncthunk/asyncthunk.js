13c964b59c740c57eb414c1697edcb49717bc32a
fernal73 <linus.fernandes@gmail.com>
2019-04-24 20:10:34 +0530

    Asyncthunk

---
+++ asyncthunk/asyncthunk.js
@@ -0,0 +1,42 @@
+/*jshint globalstrict: true*/
+/*jshint esversion: 6 */
+/*jshint node: true */
+'use strict';
+
+function asyncFlowWithThunks(generatorFunction) {
+    var generator, thunk;
+    function callback(err) {
+        if (err)
+        return generator.throw(err);
+        var results = Array.prototype.slice.call(arguments, 1),
+            thunk = generator.next(results.length > 1 ? results : results[0]).value;
+        if (thunk)
+          thunk(callback);
+    }
+    generator = generatorFunction();
+    thunk = generator.next().value;
+    if (thunk)
+      thunk(callback);
+}
+
+var fs = require('fs');
+var path = require('path');
+
+function readFileThunk(filename, options) {
+    return function(callback) {
+        fs.readFile(filename, options, callback);
+    };
+}
+
+function writeFileThunk(filename, body) {
+    return function(callback) {
+        fs.writeFile(filename, body, callback);
+    };
+}
+
+
+asyncFlowWithThunks(function*() {
+    var myself = yield readFileThunk(__filename, 'utf8');
+    yield writeFileThunk("clone_of_asyncthunk.js", myself);
+    console.log("Clone created");
+});
