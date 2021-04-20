050a482ccba2483329a80d58f11213e8697f25d0
fernal73 <linus.fernandes@gmail.com>
2021-04-20 12:45:55 +0530

    Add support for patch method

--- crud/crudapi.js
+++ crud/crudapi.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 app.get("/users", (req, res) => {
   // get all users
-  console.log(`Get all users for request: ${req} and response : ${res} \n`);
+  console.log(`Get all users for request: ${req} and response : ${res} `);
   const users = [{
     id: 1,
     name: "Emanuele"
@@ -16,22 +16,27 @@ app.get("/users", (req, res) => {
 });
 app.get("/users/:id", (req, res) => {
   // get the user with the specified id
-  console.log(`Get user id for request: ${req} and response : ${res} \n`);
+  console.log(`Get user id for request: ${req} and response : ${res} `);
   res.send("User id request received");
 });
 app.post("/users", (req, res) => {
   // create a new user
-  console.log(`create new user for request: ${req} and response : ${res} \n`);
+  console.log(`create new user for request: ${req} and response : ${res} `);
   res.send("User id post received");
 });
 app.put("/users/:id", (req, res) => {
   // update the user with specified id
-  console.log(`updating user for request: ${req} and response : ${res} \n`);
+  console.log(`updating user for request: ${req} and response : ${res} `);
   res.send("User id put received");
+});
+app.patch("/users/:id", (req, res) => {
+  // update the user with specified id
+  console.log(`patching user for request: ${req} and response : ${res} `);
+  res.send("User id patch received");
 });
 app.delete("/users/:id", (req, res) => {
   // delete the user with specified id
-  console.log(`delete user for request: ${req} and response : ${res} \n`);
+  console.log(`delete user for request: ${req} and response : ${res} `);
   res.send("User id delete received");
 });
 app.listen(8000, () => {
