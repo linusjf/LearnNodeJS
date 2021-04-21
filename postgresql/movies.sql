ecea3e04c119c8cd7b4603182df7bfaa645e5d66
fernal73 <linus.fernandes@gmail.com>
2021-04-21 17:03:31 +0530

    Add movies sql script

---
+++ postgresql/movies.sql
@@ -0,0 +1,16 @@
+drop table movies; 
+create table if not exists movies (
+  id serial primary key, 
+  title varchar(50) not null, 
+  year integer not null, 
+  created_on timestamp not null);
+insert into movies (title, year)
+values ('Her', 2013);
+insert into movies (title, year)
+values ('I, Robot', 2004);
+insert into movies (title, year)
+values ('A.I. Artificial Intelligence', 2001);
+insert into movies (title, year)
+values ('2001: A Space Odyssey', 1968);
+insert into movies (title, year)
+values ('Blade Runner', 1982);
