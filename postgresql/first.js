9909bf014ccefe84faa64d265f154ebd06ea8b68
fernal73 <linus.fernandes@gmail.com>
2021-04-22 09:45:25 +0530

    add cars.sql, first.js

---
+++ postgresql/first.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const pg = require("pg");
+const R = require("ramda");
+
+const cs = "postgres://adminpgsql:postgresql@localhost:5432/mydb";
+
+const client = new pg.Client(cs);
+client.connect().catch(err => {
+  console.log("error: ", err);
+});
+
+client.query("SELECT 1 + 4").then(res => {
+
+  const result = R.head(R.values(R.head(res.rows)));
+
+  console.log(result);
+}).catch(err => {
+  console.log("error: ", err);
+}).finally(() => client.end());
