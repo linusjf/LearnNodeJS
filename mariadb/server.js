89dc7c304649c6ec2507b128feea48a1e7894e84
fernal73 <linus.fernandes@gmail.com>
2021-04-23 13:28:20 +0530

    Add server

---
+++ mariadb/server.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const express = require("express");
+const pool = require("./connect.js");
+const app = express();
+const port = 8080;
+
+// expose an endpoint "people"
+app.get("/people", async (req, res) => {
+  let conn;
+  try {
+    // establish a connection to MariaDB
+    conn = await pool.getConnection();
+
+    // create a new query
+    var query = "select name from people";
+
+    // execute the query and set the result to a new variable
+    var rows = await conn.query(query);
+
+    // return the results
+    res.send(rows);
+  } finally {
+    if (conn)
+      conn.release();
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
