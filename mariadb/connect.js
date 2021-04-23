48e5f8517ed98b4ebbced41ea9d180cdb9931e63
fernal73 <linus.fernandes@gmail.com>
2021-04-23 14:26:59 +0530

    move db parameters to .env. add error handling.

--- mariadb/connect.js
+++ mariadb/connect.js
@@ -1,12 +1,13 @@
 // import mariadb
 var mariadb = require("mariadb");
+require("dotenv").config();
 
 // create a new connection pool
 const pool = mariadb.createPool({
-  host: "127.0.0.1",
-  user: "adminmdb",
-  password: "mariadb",
-  database: "demo"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME
 });
 
 // expose the ability to create new connections
