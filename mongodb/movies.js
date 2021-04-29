9365212fbad95233c5146fada4e52a366b4dd20e
fernal73 <linus.fernandes@gmail.com>
2021-04-29 20:18:25 +0530

    Update

--- mongodb/movies.js
+++ mongodb/movies.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const mongoose = require("mongoose");
+const Movie = require("./movieSchema");
 
 async function insertRecords() {
   await mongoose.connect("mongodb://localhost/test", {
@@ -14,47 +15,47 @@ async function insertRecords() {
     console.log("open");
   });
 
-  const Movie = require("./movieSchema");
   let movie = new Movie({
     title: "The Matrix",
     year: 1999
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
   movie = new Movie({
     title: "The Shawshank Redemption",
     year: 1994
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
   movie = new Movie({
     title: "The Godfather",
     year: 1972
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
+
   movie = new Movie({
     title: "The Godfather - Part II",
     year: 1974
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
   movie = new Movie({
     title: "The Dark Knight",
     year: 2008
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
   movie = new Movie({
     title: "12 Angry Men",
     year: 1957
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
   movie = new Movie({
     title: "Schindlers List",
     year: 1993
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
   movie = new Movie({
     title: "Pulp Fiction",
     year: 1994
   });
-  await movie.save();
+  movie.save().catch(err => console.log(err.message));
 }
 
 (async () => {
