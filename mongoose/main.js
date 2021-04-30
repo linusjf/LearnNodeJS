cfc3d17b3ea9cac6724b8401076aeaf683b42532
fernal73 <linus.fernandes@gmail.com>
2021-05-01 05:29:32 +0530

    Add more CRUD methods

--- mongoose/main.js
+++ mongoose/main.js
@@ -11,9 +11,27 @@ async function main() {
   await msg.save();
   var record = await EmailModel
     .find({
-      email: "ada.lovelace@gmail.com" 
+      email: "ada.lovelace@gmail.com"
     });
   console.log(record);
+  record = await EmailModel
+    .findOneAndUpdate({
+      email: "ada.lovelace@gmail.com"
+    }, {
+      email: "theoutlander@live.com"
+    }, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false
+    });
+  console.log(record);
+  var result = await EmailModel
+    .findOneAndRemove({
+      email: "theoutlander@live.com"
+    }, {
+      useFindAndModify: false
+    });
+  console.log(result);
 }
 
 (async () => {
