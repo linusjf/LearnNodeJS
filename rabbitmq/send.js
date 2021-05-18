42accdc0df02cca48ff9dd2daa85b4851a97dbc4
fernal73 <linus.fernandes@gmail.com>
2021-05-18 11:41:10 +0530

    Add options to dotenv

--- rabbitmq/send.js
+++ rabbitmq/send.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
 require("dotenv").config();
 const amqp = require("amqplib/callback_api");
+const options = {
+  protocol: process.env.PROTOCOL,
+  hostname: process.env.HOSTNAME,
+  port: process.env.PORT,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  locale: process.env.LOCALE,
+  frameMax: process.env.FRAMEMAX,
+  heartbeat: process.env.HEARTBEAT,
+  vhost: process.env.VHOST
+};
 
-amqp.connect(process.env.CLOUD_AMQP_URL, function(error0, connection) {
+amqp.connect(options, function(error0, connection) {
   if (error0) {
     throw error0;
   }
