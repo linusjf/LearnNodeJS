9a47f9388e81f6b3b79bd8eabd1ba61935afed9b
fernal73 <linus.fernandes@gmail.com>
2021-05-19 17:08:12 +0530

    Add invalid content listener

--- rabbitmq/rascalclient.js
+++ rabbitmq/rascalclient.js
@@ -6,18 +6,24 @@ config.vhosts.test.connection.url = process.env.CLOUD_AMQP_URL;
 
 async function rascal_consume(){
   console.log("Consuming");
-  const broker = await Broker.create(config);
-  broker.on("error", console.error);
-  const subscription = await broker.subscribe("demo_subscription", "b1");
-  subscription.on("message", (message, content, ackOrNack)=>{
-    console.log(content);
-    ackOrNack();
+  try {
+    const broker = await Broker.create(config);
+    broker.on("error", console.error);
+    const subscription = await broker.subscribe("demo_subscription", "b1");
+    subscription.on("message", (message, content, ackOrNack)=>{
+      console.log(content);
+      ackOrNack();
     //    subscription.cancel();
-  });
-  subscription.on("error", console.error);
-  subscription.on("invalid_content", (_err,_message,_ackOrNack) =>{
-    console.log("Failed to parse message");
-  });
+    }).on("invalid_content", (_err, message, _ackOrNack) => {
+      console.log("Dropping message " + message);
+    });
+    subscription.on("error", console.error);
+    subscription.on("invalid_content", (_err,_message,_ackOrNack) =>{
+      console.log("Failed to parse message");
+    });
+  } catch (err) {
+    console.error("Subscription does not exist: " + err.message);
+  }
 }
 
 rascal_consume().catch(console.error);
