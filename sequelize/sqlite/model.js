b6a9b4df2aa09ef71964c80cbe2bae3efba67e88
fernal73 <linus.fernandes@gmail.com>
2021-05-22 13:24:47 +0530

    Add methods to model

--- sequelize/sqlite/model.js
+++ sequelize/sqlite/model.js
@@ -10,39 +10,47 @@ const sequelize = new Sequelize({
 });
 
 async function start() {
-  class User extends Model {}
+  class User extends Model {
+    getFullName() {
+      return [this.firstName, this.lastName].join(" ");
+    }
+  }
 
-  await  User.init({
+  User.init({
   // Model attributes are defined here
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     lastName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      defaultValue: ""
     // allowNull defaults to true
     }
   }, {
   // Other model options go here
   // We need to pass the connection instance
-    sequelize, 
+    sequelize,
+    timestamps: true,
     // We need to choose the model name
     modelName: "User",
     tableName: "Users"
   });
 
-  // the defined model is the class itself
-  // true
+  const user = User.build({ firstName: "Jane", lastName: "Doe" });
+  console.log(user.getFullName()); 
   console.log(User === sequelize.models.User);
 
   await sequelize.authenticate();
   console.log("Connection has been established successfully.");
-  
+
   // This will run .sync() 
   // only if database name is 'mydb'
   await sequelize.sync({ force: true, 
-    match: /mydb/ });
+    match: /mydb/ ,
+    alter: true });
   console.log("Database & tables created!");
+
 
   await User.drop();
   console.log("User table dropped!");
