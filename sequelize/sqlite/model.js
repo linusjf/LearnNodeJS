9853701f1a5d070339552ba84612776e9c80bc2d
fernal73 <linus.fernandes@gmail.com>
2021-05-22 16:41:55 +0530

    Convert getters and setters to virtual fields

--- sequelize/sqlite/model.js
+++ sequelize/sqlite/model.js
@@ -9,36 +9,63 @@ const sequelize = new Sequelize({
   }
 });
 
-async function start() {
-  class User extends Model {
-    getFullName() {
-      return [this.firstName, this.lastName].join(" ");
-    }
-  }
+class User extends Model {
+}
 
-  User.init({
+User.init({
   // Model attributes are defined here
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      defaultValue: ""
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    defaultValue: ""
     // allowNull defaults to true
+  },
+  fullName: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    set(value) {
+      var arr = value.split(" ");
+      if (arr.length == 1)
+        this.firstName = arr.pop();
+      else {
+        this.lastName = arr.pop();
+        this.firstName = arr.join(" ");
+      }
     }
-  }, {
+  },
+  favoriteColor: {
+    type: DataTypes.TEXT,
+    defaultValue: "green"
+  },
+  dob: DataTypes.DATE,
+  age:{ 
+    type: DataTypes.VIRTUAL,
+    get() {
+      var diff_ms = Date.now() - this.dob.getTime();
+      var age_dt = new Date(diff_ms); 
+      return Math.abs(age_dt.getUTCFullYear() - 1970);
+    },
+  },
+  cash: DataTypes.INTEGER
+}, {
   // Other model options go here
   // We need to pass the connection instance
-    sequelize,
-    timestamps: true,
-    // We need to choose the model name
-    modelName: "User",
-    tableName: "Users"
-  });
+  sequelize,
+  timestamps: true,
+  // We need to choose the model name
+  modelName: "User",
+  tableName: "Users"
+});
 
-  const user = User.build({ firstName: "Jane", lastName: "Doe" });
-  console.log(user.getFullName()); 
+async function start() {
+  const user = User.build({ firstName: "Jane", lastName: "Doe" ,
+    dob: new Date("1969-12-23")});
+  console.log(user.get()); 
   console.log(User === sequelize.models.User);
 
   await sequelize.authenticate();
@@ -46,11 +73,10 @@ async function start() {
 
   // This will run .sync() 
   // only if database name is 'mydb'
-  await sequelize.sync({ force: true, 
+  await sequelize.sync({ force: false, 
     match: /mydb/ ,
-    alter: true });
+    alter: false});
   console.log("Database & tables created!");
-
 
   await User.drop();
   console.log("User table dropped!");
