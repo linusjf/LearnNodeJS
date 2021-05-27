32d01b59b957d2d1bd6d68ad77fdbc3b3db24873
fernal73 <linus.fernandes@gmail.com>
2021-05-27 10:37:29 +0530

    Add more lodash samples

---
+++ lodash/randomvals.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const _ = require("lodash");
+
+let r = _.random(10);
+console.log(r);
+
+r = _.random(5, 10);
+console.log(r);
---
+++ lodash/shufflefun.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const _ = require("lodash");
+
+let words = ["sky", "wood", "forest", "falcon", 
+  "pear", "ocean", "universe"];
+
+console.log(_.shuffle(words));
+console.log(_.shuffle(words));
+console.log(_.shuffle(words));
+console.log(words);
