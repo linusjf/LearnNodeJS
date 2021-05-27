689a0c5c1590c3446e33891b6c9c3b7c86fc19fb
fernal73 <linus.fernandes@gmail.com>
2021-05-27 12:00:10 +0530

    Add more lodash examples

---
+++ lodash/rangefun.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const _ = require("lodash");
+
+let vals = _.range(10);
+console.log(vals);
+
+let vals2 = _.range(0, 15);
+console.log(vals2);
+
+let vals3 = _.range(0, 15, 5);
+console.log(vals3);
