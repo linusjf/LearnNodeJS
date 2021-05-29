fadc4ad8940d8d7b23572631bea8b35fc8a06460
fernal73 <linus.fernandes@gmail.com>
2021-05-29 12:01:40 +0530

    Add http

---
+++ http/sendhtml.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const http = require("http");
+const server = http.createServer(function (req, res) {
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><p>This is home page.</p></body></html>");
+    res.end();
+  } else if (req.url == "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><p>This is contact page</p></body></html>");
+    res.end();
+  } else if (req.url == "/admin") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><p>This is admin page</p></body></html>");
+    res.end();
+  } else {
+    res.end("Invalid request");
+  }
+});
+server.listen(8080);
+console.log("server running on port 8080");
