09865ccaddee031c4165dc940ccc922efb912491
fernal73 <linus.fernandes@gmail.com>
2021-05-30 16:43:51 +0530

    Add express samples

---
+++ express/appsimple.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Hello, there!"));
+app.listen(3000, () => console.log("Application started on port 3000"));
