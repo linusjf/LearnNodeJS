91b1cd91652d8df07d9e72ee873cb171ce7110fa
fernal73 <linus.fernandes@gmail.com>
2021-05-30 19:27:33 +0530

    Add more scripts

---
+++ express/appbodyparser.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.post("/info", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.listen(3000, () => {
+  console.log("Application started on port 3000");
+});
