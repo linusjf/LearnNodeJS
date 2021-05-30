ee6602e0f1261e76f36680ff9043b298bb305e76
fernal73 <linus.fernandes@gmail.com>
2021-05-30 21:27:28 +0530

    Add more scripts

---
+++ express/appstat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Application started on port 3000");
+});
