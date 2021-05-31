3be1eb88c7825c2f83173dd5a8d7a78a15582459
fernal73 <linus.fernandes@gmail.com>
2021-05-31 16:11:33 +0530

    Add Axios examples

---
+++ axios/basicapi.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const axios = require("axios");
+
+async function makeRequest() {
+  const config = {
+    method: "get",
+    url: "http://webcode.me"
+  };
+
+  let res = await axios(config);
+  console.log(res.status);
+}
+
+makeRequest();
---
+++ axios/headreq.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const axios = require("axios");
+
+async function makeHeadRequest() {
+  let res = await axios.head("http://webcode.me");
+
+  console.log(`Status: ${res.status}`);
+  console.log(`Server: ${res.headers.server}`);
+  console.log(`Date: ${res.headers.date}`);
+}
+
+makeHeadRequest();
